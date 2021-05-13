68d6586a450f670c3f61d8ccbed59439079c07c1
Hady Mohamed <hadymohamed@pursuit.org>
2021-05-13 18:55:21 -0400

    styled and finished

--- nasa-app/src/App.js
+++ nasa-app/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import NasaPhoto from "./Components/NasaPhoto";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <Route exact path="/" component={Home} />
-      <Route path="nasaphoto" component={NasaPhoto} />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Route component={Home} path="/" exact />
+        <Route component={NasaPhoto} path="/nasaphoto" />
+      </div>
+    </BrowserRouter>
   );
 }
--- nasa-app/src/Components/Home.js
+++ nasa-app/src/Components/Home.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-    return (
-        <div className="Home">
-            <Link className="Home-Link" to="/nasaphoto">See into the stars!</Link>
-        </div>
-    )
+  return (
+    <div className="home">
+      <Link className="home-link" to="/nasaphoto">
+        See into the stars!
+      </Link>
+    </div>
+  );
 }
